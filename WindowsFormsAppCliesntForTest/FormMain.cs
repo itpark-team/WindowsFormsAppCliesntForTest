75ef08b67460d72bb8107ef177f374767222712f
itpark-team <46758840+itpark-team@users.noreply.github.com>
2022-01-23 19:11:24 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsAppCliesntForTest/FormMain.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net;
+using Newtonsoft.Json;
+using WindowsFormsAppCliesntForTest.Models;
+
+namespace WindowsFormsAppCliesntForTest
+{
+    public partial class FormMain : Form
+    {
+        private WebClient webClient;
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            webClient = new WebClient();
+            webClient.Encoding = Encoding.UTF8;
+            //webClient.Headers[HttpRequestHeader.ContentType] = "application/json";
+            webClient.Headers.Set("content-type", "application/json");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string jsonData = webClient.DownloadString("https://localhost:44361/Posts");
+
+            List<PostData> postDatas = JsonConvert.DeserializeObject<List<PostData>>(jsonData);
+
+            dataGridView1.DataSource = postDatas;
+
+            MessageBox.Show("Ok");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            PostData insertData = new PostData() { Id = 1111, UserId = 11111, Body = "sdsd123ds", Title = "fbgd23423h" };
+
+            string jsonData = JsonConvert.SerializeObject(insertData);
+
+            webClient.UploadString("https://localhost:44361/Posts", jsonData);
+
+            MessageBox.Show("Ok");
+
+        }
+    }
+}
---
+++ WindowsFormsAppCliesntForTest/Models/PostData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace WindowsFormsAppCliesntForTest.Models
+{
+    class PostData
+    {
+        [JsonProperty("userId")]
+        public int UserId { get; set; }
+
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("body")]
+        public string Body { get; set; }
+    }
+}
